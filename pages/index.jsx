fda7f072a0cacd21b49fef7689b7d3241df43d7d
LeoMdrs <leandro.mdrs06@gmail.com>
2021-09-24 21:08:07 -0300

    Criando a tela inicial/menu do jogo

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,46 @@
+import styles from "../styles/Formulario.module.css"
+import Cartao from "../components/Cartao"
+import EntradaNumerica from "../components/EntradaNumerica"
+import Link from "next/link"
+import { useState } from "react"
 
-export default function Home() {
+export default function Formulario() {
+
+  const [qtdPortas, setQtdPortas] = useState(3)
+  const [comPresente, setComPresente] = useState(1)
+
   return (
-    <div style={{ display: "flex" }}>
-      <h1>Formulário de início</h1>
+    <div className={styles.formulario}>
+      <div>
+        <Cartao bgcolor="#c0392c">
+          <h1>Monty Hall</h1>
+        </Cartao>
+
+        <Cartao>
+          <EntradaNumerica
+            text="Qtd Portas"
+            value={qtdPortas}
+            onChange={novaQtd => setQtdPortas(novaQtd)}
+          />
+        </Cartao>
+      </div>
+
+      <div>
+        <Cartao>
+          <EntradaNumerica
+            text="Qtd Portas"
+            value={comPresente}
+            onChange={novaPortaComPresente => setComPresente(novaPortaComPresente)}
+          />
+        </Cartao>
+
+        <Cartao bgcolor="#28a085">
+          <Link href={`/jogo/${qtdPortas}/${comPresente}`}>
+            <h2 className={styles.link}>Iniciar</h2>
+          </Link>
+        </Cartao>
+      </div>
+
     </div>
   )
 }
